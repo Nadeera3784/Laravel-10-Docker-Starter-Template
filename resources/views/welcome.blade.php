f218c0c3b283a1c6bc5a6b9e3b71491341e1a472
Nadeera Sampath <nadeera3784@gmail.com>
2024-07-06 05:44:22 +0530

    fix:vite build fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        @vite('resources/css/app.css')
+        @viteReactRefresh
+       @vite(['resources/sass/app.scss', 'resources/js/app.jsx'])
     </head>
     <body class="antialiased">
        <!-- Page Container -->
